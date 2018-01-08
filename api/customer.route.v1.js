2f6282fe7482dae66ab77633a467fb093fe38b68
unknown <jeffry-1997@outlook.com>
2018-01-08 14:29:17 +0100

    model customer toegevoegd

--- api/customer.route.v1.js
+++ api/customer.route.v1.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const routes = express.Router();
 const mongodb = require('../config/dbMongo');
-
+const customer = require('../model/customer.model');
 const mongoose = require('mongoose');
 
 routes.get('/test', function(req, res){
     res.contentType('application/json');
     res.status(200).send({'test':'test'})
 });
+
+routes.post('/customer', function(req, res) {
+    const customerProps = req.body;
+
+    customer.create(customerProps)
+        .then((customer) => {
+        res.status(200).send(customer)
+})
+.catch((error) => res.status(400).json(error))
+});
+
 
 module.exports = routes;
---
+++ model/customer.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    addres: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    postalCode: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+});
+
+const Customer = mongoose.model('customer', CustomerSchema);
+
+Customer.count({}, function (err, count) {
+
+    if(count < 5){
+        console.log('voeg customer toe');
+        const customer = new Customer({
+            firstName: 'Tom',
+            lastName: 'van Haaster',
+            addres: 'Geerkade 15',
+            phoneNumber: '061234567',
+            postalCode: '4871CK',
+            email: 'soldier76@hotmail.com'
+        });
+
+        customer.save();
+    }
+
+});
+
+module.exports = Customer;
